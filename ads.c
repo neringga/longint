f1f17a2f4ee8e1ac451d928da2fa21d2a5183da0
neringga <geigalaite@gmail.com>
2018-02-11 18:50:59 +0200

    Addition function is done

--- ads.c
+++ ads.c
@@ -11,25 +11,29 @@ struct el {
 // typedef struct el LIST;
 
 void create_the_list(struct el **);
-void insert_element(struct el **, int );
+void insert_element_at_the_end(struct el **, int );
+void insert_element_at_the_beggining(struct el **, int );
 void creating_longint_as_a_list (struct el **, char []);
+void check_before_addition (struct el *, struct el *, struct el **);
 void displayList(struct el *);
 
 int main ()
 {
-	char M[] = "0001234567890";
-	char M1[] = "-1234";  
+	char M[] = "-0000005678";
+	char M1[] = "-3";  
 	struct el *longNumber;
 	struct el *longNumber2;
+	struct el *sum;
 	int i;
 	
 	
 	creating_longint_as_a_list(&longNumber, M);
 	creating_longint_as_a_list(&longNumber2, M1);
+	check_before_addition(longNumber, longNumber2, &sum);
 	
 	
 	
-	displayList(longNumber);
+	displayList(sum);
 	
 	return 0;
 }
@@ -39,7 +43,7 @@ void create_the_list (struct el **head)
 	*head=NULL;
 }
 
-void insert_element(struct el **head, int n) 
+void insert_element_at_the_end(struct el **head, int n) 
 {
 	struct el *temp;
 	struct el *p;
@@ -64,6 +68,35 @@ void insert_element(struct el **head, int n)
 	
 }
 
+void insert_element_at_the_beggining(struct el **head, int n) 
+{
+	struct el *temp;
+	struct el *p;
+	
+	temp=(struct el *)malloc(sizeof(struct el));
+	temp->data = n;
+	temp->next = *head;
+	
+	*head = temp;
+	
+	// if (*head == NULL) 
+	// {
+			// *head = temp;
+	// }
+	// else 
+	// {
+		// p = *head;
+			
+		// while (p->next != NULL) 
+			// {
+				// p = p->next;
+			// }
+			
+		// p->next = temp;
+	// }
+	
+}
+
 void creating_longint_as_a_list (struct el **head, char M[])
 {
 	int i = 0;
@@ -72,19 +105,19 @@ void creating_longint_as_a_list (struct el **head, char M[])
 	
 	if ( M[i] == '-' )
 	{
-		insert_element(head, 1);			//first element of the list shows whether the number is negative or positive (1 for negative).
+		insert_element_at_the_end(head, 1);			//first element of the list shows whether the number is negative or positive (1 for negative).
 		i = 1;
 	}
 	else 
 	{
-		insert_element(head, 0);			// 0 for positive
+		insert_element_at_the_end(head, 0);			// 0 for positive
 	}
 	
 	while (i < strlen(M))
 	{
 		if ((M[i] != '0') || (first_zeros == 1) || ( i+1 == strlen(M)) )         //when person enters number with zeros at the beggining
 		{
-			insert_element(head, M[i] - 48);
+			insert_element_at_the_end(head, M[i] - 48);
 			i++;
 		}
 		else
@@ -95,6 +128,141 @@ void creating_longint_as_a_list (struct el **head, char M[])
 				first_zeros = 1;
 			}
 		}
+	}
+	
+}
+
+void addition (struct el *a, struct el *b, struct el **sum)
+{
+	struct el *temp = a;
+	struct el *temp2 = b;
+	int lenght = 0, lenght2 = 0, temp_lenght, i, sum1, overflow = 0, n;
+	struct el *temp3;
+	
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		lenght++;
+	}
+	while (temp2 != NULL)
+	{
+		temp2 = temp2->next;
+		lenght2++;
+	}
+	
+	if (lenght < lenght2)                     //a has to be longer than b, lenght has to be longer than lenght2
+	{
+		temp3 = a;
+		a = b;
+		b = temp3;
+		temp_lenght = lenght;
+		lenght = lenght2;
+		lenght2 = temp_lenght;
+	}
+	
+	
+	
+	while ( (lenght !=1) && (lenght2 !=1) )
+	{
+		temp = a;
+		temp2 = b;
+		
+		for (i=0; i<lenght-1; i++) 
+		{
+			temp = temp->next;
+		}
+		
+		for (i=0; i<lenght2-1; i++)
+		{
+			temp2 = temp2->next;
+		}
+					
+		sum1 = temp->data + temp2->data + overflow;
+		
+		if (sum1 > 10)
+		{
+			overflow = 1;
+			n = sum1 % 10;
+		}
+		else
+		{
+			overflow = 0;
+			n = sum1;
+		}
+		insert_element_at_the_beggining (sum, n);
+
+		lenght--;
+		lenght2--;
+		
+	}
+
+	if (lenght != 1)
+	{
+		while (lenght != 1)
+		{
+			temp = a;
+			
+		for (i=0; i<lenght-1; i++) 
+		{
+			temp = temp->next;
+		}
+			
+		sum1 = temp->data + overflow;
+		
+		if (sum1 == 10)
+		{
+			overflow = 1;
+			n = 0;
+		}
+		else
+		{
+			overflow = 0;
+			n = sum1;
+		}
+		insert_element_at_the_beggining(sum, n);
+		lenght--;
+		}
+	}
+	
+}
+
+void check_before_addition (struct el *longNumber, struct el *longNumber2, struct el **sum)
+{	
+	create_the_list(sum);
+	*sum=(struct el *)malloc(sizeof(struct el));
+	(*sum)->next = NULL;
+	
+	if ( (longNumber->data == 0) && (longNumber2->data == 0) || ((longNumber->data == 1) && (longNumber2->data == 1)) )
+	{
+
+		addition(longNumber, longNumber2, sum);
+		
+		if (longNumber->data == 0)
+		{
+			insert_element_at_the_beggining(sum, 0);
+		}
+		else
+		{
+			insert_element_at_the_beggining(sum, 1);
+		}
+		
+	}
+	else
+	{
+		
+		if ( longNumber->data == 0 )
+		{
+			longNumber2->data = 0;
+			subtraction(longNumber, longNumber2, sum);
+			longNumber2->data = 1;
+		}
+		else
+		{
+			longNumber->data = 0;
+			subtraction(longNumber, longNumber2, sum);
+			longNumber->data = 1;
+		}
+		
 	}
 	
 }
@@ -110,9 +278,9 @@ void displayList(struct el *head)
 	else 
 	{
 		
-		while (p != NULL)
+		while (p->next != NULL)
 			{
-			printf ("%d", p->data);
+			printf ("%d ", p->data);
 			p = p->next;
 			}
 			
